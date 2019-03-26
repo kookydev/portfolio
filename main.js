9137bd892db90c654307a350813bc1fb95203bfa
kookydev <info@kooky.dev>
2019-03-26 12:59:08 +0000

    tyime to go take mumm to hospital

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-let menuCollapsed = false;
 let nav = document.getElementById("nav");
 let navList = document.querySelector("nav-list");
+let navToggle = document.getElementById("nav-toggle")
+let cursor = document.getElementById("cursor")
+let logoItems = document.getElementsByClassName("nav-logo")
+let logo1 = document.getElementById("logo1")
+let logo3 = document.getElementById("logo3")
+let logo4 = document.getElementById("logo4")
+let logo5 = document.getElementById("logo5")
+let logo6 = document.getElementById("logo6")
+let logoArray = [logo1, logo3, logo4, logo5, logo6]
 
+navToggle.checked = true;
 
 function checkboxToggle(el) {
   if (el.checked) {
     nav.style.width = "100%"
+    nav.style.borderRadius = "0px"
+    cursor.style.order = 1
+    logoArray.map(i => i.style.display = "block")
   } else {
-    nav.style.width = "auto"
+    nav.style.width = "85px"
+    nav.style.borderRadius = "0px 0px 15px 0px"
+    cursor.style.order = 0
+    logoArray.map(i => i.style.display = "none")
   }
 }
 
-// window.onscroll = function () {
-//   scrollTrigger()
-// };
-let navClassCheck = nav.classList.contains("nav-collapsed");
-console.log(navClassCheck);
+
+
 
 window.addEventListener("scroll", () => {
   let scrolled = window.scrollY
-  scrolled > 90 ? scrollTrigger("close") : scrollTrigger("open")
+  if (scrolled > 90) {
+    navToggle.checked = false
+    checkboxToggle(navToggle)
+  } else {
+    navToggle.checked = true
+    checkboxToggle(navToggle)
+  }
+
 })
 
 const menuAnimation = () => {
